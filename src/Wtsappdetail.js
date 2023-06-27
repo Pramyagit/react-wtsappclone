b43c61504abc9dacdbe76be691159a462f8f88e0
Pramyagit <prlivetime@gmail.com>
2023-06-27 11:00:24 +0530

    clones

--- src/Wtsappdetail.js
+++ src/Wtsappdetail.js
@@ -1,14 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { HiDotsVertical } from "react-icons/hi";
+import { FcSettings} from "react-icons/fc";
 const Wtsappdetail = () => {
   return (
     <div class="detail">
      <section className='condition'>
-<p></p>
+      <h2>Use WhatsApp on your computer</h2>
+<ul className='Lists'>
+  <li>Open WhatsApp on your phone</li><br></br>
+  <li>Tap<span>Menu</span><HiDotsVertical/> or<span>Setting</span><FcSettings/>and select <span>Linked Devices</span></li><br></br>
+  <li>Tap on <span>Link a device</span></li><br></br>
+  <li>Point your phone to this screen to capture the QR code</li><br></br>
+</ul>
      </section>
 <section class="qrcode">
-
+  <img src=""alt='image'></img>
 </section>
+{/* <hr></hr> */}
+{/* <Link to="/phone">Link with phone number</Link> */}
 
     </div>
   )
